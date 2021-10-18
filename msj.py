c15ce57ba1522589c20c7353d98377fef2664016
tibet5 <59174238+tibet5@users.noreply.github.com>
2021-10-19 00:55:02 +0300

    tokens deleted

--- msj.py
+++ msj.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client 
  
-account_sid = 'AC7fab25b030d116b710155ccf9a906e08' 
-auth_token = 'dcdf363fe4f4fec1f75565db7daf7764' 
+account_sid = '#####################' 
+auth_token = '#####################' 
 client = Client(account_sid, auth_token) 
 
 def mesaj():
